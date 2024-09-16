72827a08ffa19fc8f1519547012cfc7e2c43d222
adlamas <alan12394@hotmail.com>
2024-09-16 20:23:58 -0300

    Validates associations and uniqueness on models

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -7,5 +7,13 @@ RSpec.describe Brand, type: :model do
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
+    context 'when creating two brands with the same name' do
+      let(:brand) { create(:brand) }
+      let(:invalid_brand) { build(:brand, name: brand.name) }
+
+      it 'raises an exception when trying to create an invalid brand' do
+        expect { invalid_brand.save! }.to raise_exception(ActiveRecord::RecordInvalid)
+      end
+    end
   end
 end
--- spec/models/car_model_spec.rb
+++ spec/models/car_model_spec.rb
@@ -7,5 +7,13 @@ RSpec.describe CarModel, type: :model do
 
   describe 'validations' do
     it { is_expected.to validate_presence_of(:name) }
+    context 'when creating two car models for a brand with the same name' do
+      let(:car_model) { create(:car_model) }
+      let(:invalid_car_model) { build(:car_model, brand: car_model.brand, name: car_model.name) }
+
+      it 'raises an exception when trying to create an invalid car_model' do
+        expect { invalid_car_model.save! }.to raise_exception(ActiveRecord::RecordInvalid)
+      end
+    end
   end
 end
